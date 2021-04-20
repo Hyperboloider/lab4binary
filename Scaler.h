8c676ed05e8dc5b2e59eb1ffb17e66882a87ec66
Ilya Knyazev <kknyaz033@gmail.com>
2021-04-20 15:45:43 +0300

    new chanel interpolation

--- Scaler.h
+++ Scaler.h
@@ -6,6 +6,9 @@ class Scaler
 {
 	T* scale_fun(T* audio, int length,float scale);
 	T interpolate(int32_t x0, T y0, int32_t x1, T y1, float x);
+	int8_t getRight(int16_t);
+	int8_t getLeft(int16_t);
+	int16_t joinCh(int8_t, int8_t);
 public:
 	T* scale_track(T* audio, int num_channels, float scale, int sample_size, int samples_count);
 	//template <typename T>
@@ -22,6 +25,29 @@ T* Scaler<T>::scale_track(T* audio, int num_channels, float scale, int sample_si
 		return new_data;
 	}
 	else if (num_channels == 2) {
+		int insertPosition = 0;
+		int  out_samples = samples_count * scale;
+		T* new_data = new T[out_samples];
+		
+		for (float i = 0; i < samples_count; i += 1/scale ) {
+			int index_prev = int(i);
+			int index_next = int(i) + 1;
+
+			int8_t prevL = getLeft(audio[index_prev]);
+			int8_t prevR = getRight(audio[index_prev]);
+			int8_t nextL = getLeft(audio[index_next]);
+			int8_t nextR = getRight(audio[index_next]);
+
+			int8_t newL = interpolate(index_prev, prevL, index_next, nextL, i);
+			int8_t newR = interpolate(index_prev, prevR, index_next, nextL, i);
+
+			int16_t newValue = joinCh(newL, newR);
+
+			new_data[insertPosition] = newValue;
+			insertPosition++;
+		}
+
+		/*
 		int channel_len = samples_count / 2;
 		T* left_channel = new T[channel_len];
 		T* right_channel = new T[channel_len];
@@ -37,6 +63,7 @@ T* Scaler<T>::scale_track(T* audio, int num_channels, float scale, int sample_si
 			new_data[2 * i] = new_left_channel[i];
 			new_data[2 * i + 1] = new_right_channel[i];
 		}
+		*/
 		return new_data;
 	}
 }
@@ -57,4 +84,19 @@ T* Scaler<T>::scale_fun(T* audio, int length, float scale) {
 template <typename T>
 T Scaler<T>::interpolate(int32_t x0, T y0, int32_t x1, T y1, float x) {
 	return y0 + (y1 - y0) * (x - x0) / (x1 - x0);
+}
+
+template <typename T>
+int8_t Scaler<T>::getLeft(int16_t val) {
+	return static_cast<int8_t>(val >> 8);
+}
+
+template <typename T>
+int8_t Scaler<T>::getRight(int16_t val) {
+	return static_cast<int8_t>(val & 0xff);
+}
+
+template <typename T>
+int16_t Scaler<T>::joinCh(int8_t left, int8_t right) {
+	return static_cast<int16_t>((left << 8) + (right & 0x00ff));
 }
